5bdad25bc5d5e7e396784156abb66626f3bd26af
Dmitry Kochanov <dmitrykochanov1@gmail.com>
2019-06-10 19:57:26 +0400

    initial

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from "vuetify";
+import colors from 'vuetify/es5/util/colors';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Vuetify, {
+    theme: {
+        primary: colors.green.darken1,
+        secondary: colors.red.lighten4,
+        accent: colors.indigo.base
+    }
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
